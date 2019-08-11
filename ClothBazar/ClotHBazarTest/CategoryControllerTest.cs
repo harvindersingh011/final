327c562a43eaf7de680944cc192eb21e06c06dc7
harvinder singh <harvindersingh.hs400@gmail.com>
2019-08-11 17:01:49 -0400

    hii

---
+++ ClothBazar/ClotHBazarTest/CategoryControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClothBazar.Web.Controllers;
+using ClothBazar.Web.Models;
+using System.Web.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ClotHBazarTest
+{
+    [TestClass]
+    public class CategoryControllerTest
+    {
+        [TestMethod]
+        public void TestCategoryTable()
+        {
+            var controller = new CategoryController();
+
+        }
+    }
+}
